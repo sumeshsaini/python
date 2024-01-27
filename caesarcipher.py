ed9d30349cdfa0d21adb37d7c6052928b0103618
Sumesh Saini <136578183+sumeshsaini@users.noreply.github.com>
2024-01-27 11:59:22 +0530

    Add files via upload

---
+++ caesarcipher.py
@@ -0,0 +1,46 @@
+logo = """           
+ ,adPPYba, ,adPPYYba,  ,adPPYba, ,adPPYba, ,adPPYYba, 8b,dPPYba,  
+a8"     "" ""     `Y8 a8P_____88 I8[    "" ""     `Y8 88P'   "Y8  
+8b         ,adPPPPP88 8PP"""""""  `"Y8ba,  ,adPPPPP88 88          
+"8a,   ,aa 88,    ,88 "8b,   ,aa aa    ]8I 88,    ,88 88          
+ `"Ybbd8"' `"8bbdP"Y8  `"Ybbd8"' `"YbbdP"' `"8bbdP"Y8 88   
+            88             88                                 
+           ""             88                                 
+                          88                                 
+ ,adPPYba, 88 8b,dPPYba,  88,dPPYba,   ,adPPYba, 8b,dPPYba,  
+a8"     "" 88 88P'    "8a 88P'    "8a a8P_____88 88P'   "Y8  
+8b         88 88       d8 88       88 8PP""""""" 88          
+"8a,   ,aa 88 88b,   ,a8" 88       88 "8b,   ,aa 88          
+ `"Ybbd8"' 88 88`YbbdP"'  88       88  `"Ybbd8"' 88          
+              88                                             
+              88           
+"""
+print(logo)
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'
+            , 'i', 'j', 'k', 'l', 'm', 'n', 'o', 
+            'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'
+            , 'i', 'j', 'k', 'l', 'm', 'n', 'o', 
+            'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+direction = input("Encode or Decode : ").lower()
+text = input("Enter your text : ").lower()
+shift = int(input("Enter the shift code : "))
+def encrypt(plain_text,shift_number):
+    cipher_text = ""
+    for letter in plain_text :
+        position = alphabet.index(letter)
+        new_position = position + shift_number
+        new_letter = alphabet[new_position]
+        cipher_text+= new_letter
+    print(f"The encode text is {cipher_text}")
+def decrypt (cipher_text,shift_number):
+    plain_text = ""
+    for letter in cipher_text :
+        position = alphabet.index(letter)
+        new_position = position - shift
+        plain_text += alphabet[new_position]
+    print(f"The decode text is {plain_text}")
+if direction == 'encode' :
+    encrypt(text,shift)
+elif direction == 'decode' :
+    decrypt(text,shift)
