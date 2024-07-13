798126a05d6c3a1f0347f8b6419b7f0d32272ab4
Sumesh Saini <136578183+sumeshsaini@users.noreply.github.com>
2024-07-13 16:55:27 +0530

    Add files via upload

---
+++ mymod4.py
@@ -0,0 +1,7 @@
+def finc():
+    print("FUNC() IN ONE.PY")
+print("Top level in python ONE.py")
+if __name__ =="__main__":
+    print("One PY is running directly!")
+else:
+    print("One PY have been imported ")
